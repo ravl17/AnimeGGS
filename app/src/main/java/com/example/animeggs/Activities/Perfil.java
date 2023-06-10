54972bb8250e4f61a36c44101dde638826b6650c
ravl17 <ravlgg511@gmail.com>
2023-06-11 00:46:35 +0200

    Cerrar Sesion perfil

--- app/src/main/java/com/example/animeggs/Activities/Perfil.java
+++ app/src/main/java/com/example/animeggs/Activities/Perfil.java
@@ -1,12 +1,17 @@
 package com.example.animeggs.Activities;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.animeggs.Adapters.BarraBusquedaHelper;
 import com.example.animeggs.Adapters.NavigationBarHelper;
 import com.example.animeggs.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Perfil extends AppCompatActivity {
 
@@ -17,9 +22,22 @@ public class Perfil extends AppCompatActivity {
         setContentView(R.layout.activity_perfil);
 
 
-        NavigationBarHelper.setupNavigationBar(this,R.id.page_3);
+        NavigationBarHelper.setupNavigationBar(this, R.id.page_3);
 
+        Button btnCerrarSesion = findViewById(R.id.btnCerrarSesion);
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent intent = new Intent(view.getContext(), Login.class);
 
+                view.getContext().startActivity(intent);
+                ((Activity) view.getContext()).overridePendingTransition(0, 0);
+                ((Activity) view.getContext()).finish();
+
+            }
+        });
 
     }
 
